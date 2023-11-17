7986d7e6d467364ca05be773f75e23d33702ef84
Marla <nosta.san@gmail.com>
2023-11-17 06:51:19 +0300

    py 6 lab - done

---
+++ 5_term/Python_prog/Labs/l6/lab_6_2.py
@@ -0,0 +1,60 @@
+# Задание №2
+# Получите содержимое 10 сайтов. Выполните задание в одном и в нескольких
+# потоках. Сравните результаты и время выполнения программы.
+
+urls = [
+    "https://www.google.com/",
+    "https://dzen.ru/",
+    "https://music.youtube.com/",
+    "https://youtube.com/",
+    "https://music.yandex.ru/",
+    "https://myfuturama.top/",
+    "https://github.com/",
+    "https://web.whatsapp.com/",
+    "https://docs.python.org/3/",
+    "https://miro.com/ru/"
+]
+
+from urllib.request import urlopen
+from time import perf_counter
+from threading import Thread
+
+
+
+def task(url, id):
+    print(f'\nНачинаем выполнение задачи {id} {url}...')
+    u = urlopen(url)
+    mysite = u.read()    
+    print(f'Задача {id} выполнена')
+
+
+def one_thread():
+    x = 0;
+    for url in urls:
+        x+=1
+        task(url,x)
+
+        
+def many_threads():
+    threads = []
+    for n in range(1, 11):
+        t = Thread(target=task, args=(urls[n-1],n,))
+        threads.append(t)
+        t.start()
+
+    for t in threads:
+        t.join()
+
+
+start_time = perf_counter()
+one_thread()
+end_time = perf_counter()
+
+start_time2 = perf_counter()
+many_threads()
+end_time2 = perf_counter()
+
+
+
+print(f'\n\nПоследовательное выполнение заняло {end_time - start_time: 0.2f} секунд.')
+print(f'Параллельное выполнение заняло {end_time2 - start_time2: 0.2f} секунд.')
---
+++ 5_term/Python_prog/Labs/l6/lab_6_3.py
@@ -0,0 +1,62 @@
+# Задание №3
+# Выполните поиск содержимого текущей директории (включая поддиректории) и
+# найдите все файлы с расширением txt в которых есть ключевое слово key.
+# Выполните задание в одном и в нескольких потоках.
+
+import os
+import sys
+from time import perf_counter
+from threading import Thread
+
+files_txt = []
+files_with_key = []
+
+#for root, dirs, files in os.walk(os.path.dirname(os.path.abspath(__file__))):
+for root, dirs, files in os.walk("/Users/nosta/Documents/study"):
+    for file in files:
+        if file.endswith(".txt"):
+             files_txt.append(os.path.join(root, file))
+
+
+def findFile(filename):
+    with open(filename, "r") as file:
+        content = file.read()
+        if "key" in content:
+            files_with_key.append(filename)
+
+def one_thread():
+    for file in files_txt:
+        findFile(file)
+
+
+def many_threads():
+    threads = []
+    for file in files_txt:
+        t = Thread(target=findFile, args=(file,))
+        threads.append(t)
+        t.start()
+
+    for t in threads:
+        t.join()
+
+
+start_time = perf_counter()
+one_thread()
+end_time = perf_counter()
+
+files_with_key.clear()
+
+start_time2 = perf_counter()
+many_threads()
+end_time2 = perf_counter()
+
+
+
+
+print(f'\n\nПоследовательное выполнение заняло {end_time - start_time: 0.6f} секунд.')
+print(f'Параллельное выполнение заняло {end_time2 - start_time2: 0.6f} секунд.')
+
+# if len(sys.argv) > 1:
+#     one_thread()
+# else:
+#     many_threads()
